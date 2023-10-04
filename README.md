bc0184c00cd4c5950b4948c6b735bc20a673b17e
Rob Agar <rob@engineeredarts.co.uk>
2023-10-04 09:25:59 +0100

    simple signal server

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Minimal GStreamer WebRTC
 
 A self contained WebRTC system that can be used to replicate GStreamer issues and attached to bug reports.
+
+## signal_server
+
+A very simple signal server.
+
+- Accepts WebSocket connections on two ports, 10001 and 10002
+- Forwards all messages received on one port to the other
--- signal_server/src/main.rs
+++ signal_server/src/main.rs
@@ -4,28 +4,47 @@ use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::broadcast;
 use tungstenite::protocol::Message;
 
+type Sender = Arc<broadcast::Sender<String>>;
+
 #[tokio::main]
 async fn main() {
     println!("Signal Server");
 
-    let (tx_1, rx_1) = broadcast::channel::<String>(16);
+    let (tx_1, _rx_1) = broadcast::channel::<String>(16);
+    let (tx_2, _rx_2) = broadcast::channel::<String>(16);
+
     let tx_1 = Arc::new(tx_1);
-    listen("127.0.0.1:10001", tx_1.clone()).await;
+    let tx_2 = Arc::new(tx_2);
+
+    tokio::select! {
+        _ = listen("backend", "127.0.0.1:10001", tx_1.clone(), tx_2.clone()) => {}
+        _ = listen("frontend", "127.0.0.1:10002", tx_2.clone(), tx_1.clone()) => {}
+    }
 }
 
-async fn listen(address: &str, tx: Arc<broadcast::Sender<String>>) {
-    println!("LISTEN {address}");
+async fn listen(label: &'static str, address: &str, tx_forward: Sender, tx_send: Sender) {
+    println!("[{label}] LISTEN {address}");
 
     let server = TcpListener::bind(address).await.unwrap();
 
     while let Ok((socket, _)) = server.accept().await {
-        tokio::spawn(handle_connection(socket, tx.subscribe()));
+        tokio::spawn(handle_connection(
+            label,
+            socket,
+            tx_forward.clone(),
+            tx_send.subscribe(),
+        ));
     }
 }
 
-async fn handle_connection(socket: TcpStream, mut rx: broadcast::Receiver<String>) {
+async fn handle_connection(
+    label: &'static str,
+    socket: TcpStream,
+    tx_forward: Sender,
+    mut rx_send: broadcast::Receiver<String>,
+) {
     let address = socket.peer_addr().unwrap();
-    println!("CONNECTION from {address}");
+    println!("[{label}] CONNECTION from {address}");
 
     // websocket handshake
     let ws_stream = tokio_tungstenite::accept_async(socket).await.unwrap();
@@ -38,17 +57,18 @@ async fn handle_connection(socket: TcpStream, mut rx: broadcast::Receiver<String
                 match m {
                     Ok(m) => match m {
                         Message::Text(text) => {
-                            println!("RECEIVED {text}")
+                            println!("[{label}] FORWARD {text}");
+                            tx_forward.send(text).unwrap();
                         }
                         _ => {}
                     }
                     Err(e) => {
-                        println!("receive error {e:?}")
+                        println!("[{label}] receive error {e:?}")
                     }
                 }
             }
-            Ok(text) = rx.recv() => {
-                println!("SEND {text}");
+            Ok(text) = rx_send.recv() => {
+                println!("[{label}] SEND {text}");
                 write.send(Message::Text(text)).await.unwrap();
             }
         }
