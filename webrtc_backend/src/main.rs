a895f51427099d94f181a568d8ce369d62ba1174
Rob Agar <rob@engineeredarts.co.uk>
2023-10-04 10:39:02 +0100

    backend send & receive signals

--- webrtc_backend/src/main.rs
+++ webrtc_backend/src/main.rs
@@ -1,7 +1,29 @@
+use tokio::sync::mpsc;
+
 mod signals;
 
-fn main() {
+use signals::Signal;
+
+#[tokio::main]
+async fn main() {
     println!("Minimal GStreamer WebRTC - Backend");
 
-    signals::connect("ws://127.0.0.1:10001");
+    let (incoming_signals_tx, mut incoming_signals_rx) = mpsc::unbounded_channel::<Signal>();
+    let (_outgoing_signals_tx, outgoing_signals_rx) = mpsc::unbounded_channel::<Signal>();
+
+    signals::connect(
+        "ws://127.0.0.1:10001",
+        incoming_signals_tx,
+        outgoing_signals_rx,
+    )
+    .await;
+
+    loop {
+        tokio::select! {
+            signal = incoming_signals_rx.recv() => {
+                println!("SIGNAL {signal:?}");
+
+            }
+        }
+    }
 }
--- webrtc_backend/src/signals.rs
+++ webrtc_backend/src/signals.rs
@@ -1,18 +1,61 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+
+use futures_util::{SinkExt, StreamExt};
+use serde::{Deserialize, Serialize};
+use tokio::sync::mpsc;
 use tungstenite::Message;
 use url::Url;
 
-pub fn connect(url: &str) {
+pub type SignalSender = mpsc::UnboundedSender<Signal>;
+pub type SignalReceiver = mpsc::UnboundedReceiver<Signal>;
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(tag = "type")]
+pub enum Signal {
+    #[serde(rename = "webrtc_offer")]
+    WebRtcOffer { data: WebRtcOfferData },
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct WebRtcOfferData {
+    sdp: String,
+}
+
+pub async fn connect(
+    url: &str,
+    incoming_signals_tx: SignalSender,
+    mut outgoing_signals_rx: SignalReceiver,
+) {
     println!("[Signals] connecting to signal server {url}");
 
     let url = Url::parse(url).unwrap();
-    let (mut socket, _response) = tungstenite::connect(url).unwrap();
+    let (socket, _response) = tokio_tungstenite::connect_async(url).await.unwrap();
 
     println!("[Signals] CONNECTED");
 
-    socket.send(Message::Text("hello".into())).unwrap();
+    let (mut ws_write, mut ws_read) = socket.split();
 
-    loop {
-        let msg = socket.read().unwrap();
-        println!("[Signals] RECEIVED {msg:?}");
-    }
+    let _task = tokio::spawn(async move {
+        loop {
+            tokio::select! {
+                Some(msg) = ws_read.next() => {
+                    match msg.unwrap() {
+                        Message::Text(text) => {
+                            println!("[Signals] RECEIVE {text}");
+                            let signal: Signal = serde_json::from_str(&text).unwrap();
+                            incoming_signals_tx.send(signal).unwrap();
+                        }
+                        _ => {}
+                    }
+
+                }
+                Some(signal) = outgoing_signals_rx.recv() => {
+                    let text = serde_json::to_string(&signal).unwrap();
+                    println!("[Signals] SEND {text}");
+                    ws_write.send(Message::Text(text)).await.unwrap();
+                }
+            }
+        }
+    });
 }
